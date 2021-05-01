80f5065a879b87b0c75dddfd72212e224f26c99c
Sascha <sascha.schwarzhaupt@gmail.com>
2021-05-01 11:18:57 +0200

    fixed it

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose")
 
 const port = 3000
 const staticRouter = require("./routes/staticRouter.js")
-const url = "mongodb+srv://Barbara:0000@cluster0.lkv3a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/sport"
+const url = "mongodb+srv://Barbara:0000@cluster0.lkv3a.mongodb.net/sport?retryWrites=true&w=majority"
 
 app.use(express.json())
 app.use("/",staticRouter)
