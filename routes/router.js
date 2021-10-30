4468e9c378d56adc8ed2f6f8d46d46ceb65cb6fe
Sascha <sascha.schwarzhaupt@gmail.com>
2021-10-31 00:32:01 +0200

    CategoryDiv now colaped at start, expand on hover

--- routes/router.js
+++ routes/router.js
@@ -30,7 +30,9 @@ app.post("/saveExerciseData", (req,res)=>{
 })
 
 app.post("/newExercise", (req,res)=>{
-    configModel.findOneAndUpdate({user:req.body.user},{exercise:req.body.exercise},{new:true},(err,created)=>{
+    configModel.findOneAndUpdate(
+        {user:req.body.user},{exercise:req.body.exercise},
+        {new:true,useFindAndModify:true},(err,created)=>{
         if (err) { res.send({err}) } else { res.send(JSON.stringify(created)) }
     })
 })
